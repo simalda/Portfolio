176b6e0fea90025c13c417d62421116d855153f7
Sofya Kolodizner <simalda83@gmail.com>
2020-10-28 15:10:14 +0200

    Improvment

--- JS/imageSlideShow.js
+++ JS/imageSlideShow.js
@@ -7,8 +7,11 @@
     var clsDemo = "demo"+j;
     console.log(cls,clsDemo);
     try{
-    showSlides(slideIndex,cls,clsDemo);   }
-    catch(e){
+      showSlides(slideIndex,cls,clsDemo);  
+       setInterval( showSlides(slideIndex+1, cls, clsDemo), 2000); // Change image every 2 seconds
+    
+  }
+   catch(e){
 console.log(e);
     }
   }
@@ -35,17 +38,24 @@ console.log(e);
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+    // captionText.innerHTML = dots[slideIndex-1].alt;
+   
   }
 
-  // function CreateProject(....){
-  //   return {
-  //     firstName:"dg",
-  //     lastName:''
-  //   }
+  
 
-  //   function ProjectToDiv(){
-
+  // var slideIndex = 0;
+  // carousel();
+  
+  // function carousel() {
+  //   var i;
+  //   var x = document.getElementsByClassName("mySlides");
+  //   for (i = 0; i < x.length; i++) {
+  //     x[i].style.display = "none";
   //   }
+  //   slideIndex++;
+  //   if (slideIndex > x.length) {slideIndex = 1}
+  //   x[slideIndex-1].style.display = "block";
+  //   setTimeout(carousel, 2000); // Change image every 2 seconds
   // }
   
--- JS/manager.js
+++ JS/manager.js
@@ -1,13 +1,40 @@
-class Manager {
-    constructor(projects) {
-      this.projects = projects;
+function copy() {
+  var mailText = encodeMail();                     
+  navigator.clipboard.writeText(mailText).then(function() {
+    var a = document.getElementById("mail")
+    if (a.querySelector("span") == null){
+      a.appendChild(createSpan("Email copied!"));
+      setTimeout(remove,1000)
     }
-    // Getter
-    get projects() {
-      return this.projects;
-    }
-    // Method
-    buildProject() {
-      return this.height * this.width;
-    }
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }, function() {
+    /* clipboard write failed */
+  });
+ 
+  
+}
+
+function createSpan(text){
+  var span = document.createElement("SPAN");
+  span.setAttribute("class", "popuptext")       
+  span.setAttribute("id", "myPopup")                  
+  var t = document.createTextNode(text);     
+  span.appendChild(t);            
+  return span                          
+
+}
+function remove(){
+  var a = document.getElementById("mail")
+  var span = document.getElementById("myPopup")
+  a.removeChild(span);
+}
+function encodeMail(){
+  var mail = "tjnbmeb94Ahnbjm/dpn"
+  var coded = ''
+  for(i=0; i<mail.length ; i++){
+     coded += String.fromCharCode(mail.charCodeAt(i)-1);
   }
+  return coded
+}
+ 
